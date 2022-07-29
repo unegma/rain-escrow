3a1a3627ddef64b8c6eaaaa72427a043b924d08e
Timothy Coleman <timhc22@gmail.com>
2022-07-29 19:25:25 +0100

    Tweaks and changes

--- src/components/panels/EscrowClaimModal.tsx
+++ src/components/panels/EscrowClaimModal.tsx
@@ -29,7 +29,7 @@ type modalProps = {
 }
 
 export default function EscrowClaimModal({
-    modalOpen, setModalOpen,
+    modalOpen, setModalOpen, tokenSymbol,
     buttonLock,
     initiateClaim, consoleData, consoleColor,
   } : modalProps )
